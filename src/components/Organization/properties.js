801cf9de09780cc500afac048cdfba7898c4c361
Josifha Ashmi J <65861420+Josifha@users.noreply.github.com>
2023-09-17 16:28:09 +0530

    Update properties.js

--- src/components/Organization/properties.js
+++ src/components/Organization/properties.js
@@ -2,8 +2,8 @@ export const properties = {
     base_url: "https://na-1-dev.api.opentext.com",
     css_url: "https://css.na-1-dev.api.opentext.com",
     tenant_id: "686596a5-0c8c-4afc-96b8-d408e905c989",
-    username: "josifhaashmij77@gmail.com",
-    password: "Jo@123456789",
+    username: "security purpose",
+    password: "security Purpose",
     client_id: "DEdSE0xFU3x8B3YjBTnsp1Ek0oQ375gx",
     client_secret: "wgGYk9PL126fSD9S"
 }
